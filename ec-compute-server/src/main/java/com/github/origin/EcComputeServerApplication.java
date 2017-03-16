3308618cb6b28943d073396428063518eda4b93a
zhuguidongChina <TZzhuguidong@outlook.com>
2017-03-16 17:38:59 +0800

    电商后台服务提供方

---
+++ ec-compute-server/src/main/java/com/github/origin/EcComputeServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.origin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcComputeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcComputeServerApplication.class, args);
+	}
+}
