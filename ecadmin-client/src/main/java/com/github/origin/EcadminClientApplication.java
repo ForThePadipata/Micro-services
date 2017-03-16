b68d9d0cb61d7ed340dd77d20388f79823c4298e
zhuguidongChina <TZzhuguidong@outlook.com>
2017-03-16 17:42:46 +0800

    电商管理系统客户端添加

---
+++ ecadmin-client/src/main/java/com/github/origin/EcadminClientApplication.java
@@ -0,0 +1,12 @@
+package com.github.origin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcadminClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcadminClientApplication.class, args);
+	}
+}
