8e60cb64ba26f7f710f20c1bebed74a3d30615cf
zhuguidongChina <TZzhuguidong@outlook.com>
2017-03-17 18:15:35 +0800

    更新配置文件 添加注解

--- ecadmin-client/src/main/java/com/github/origin/EcadminClientApplication.java
+++ ecadmin-client/src/main/java/com/github/origin/EcadminClientApplication.java
@@ -2,7 +2,11 @@ package com.github.origin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class EcadminClientApplication {
 
