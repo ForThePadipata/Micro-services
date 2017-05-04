cbf4eae66d51c71d466d4ca3b3a28b6c8640c1fc
zhuguidongChina <TZzhuguidong@outlook.com>
2017-05-04 18:05:26 +0800

    添加新的公共模型类 方便数据交互

---
+++ ec-entity/src/main/java/com/github/origin/common/CommonModel.java
@@ -0,0 +1,52 @@
+package com.github.origin.common;
+
+import java.util.List;
+
+/**
+ * Created by Zhu on 2017/5/4.
+ */
+public class CommonModel {
+
+	private Object dataObject;
+
+	private List<Object> dataList;
+
+	private String errorCode;
+
+	private String errorMessage;
+
+	public Object getDataObject() {
+		return dataObject;
+	}
+
+	public void setDataObject(Object dataObject) {
+		this.dataObject = dataObject;
+	}
+
+	public List<Object> getDataList() {
+		return dataList;
+	}
+
+	public void setDataList(List<Object> dataList) {
+		this.dataList = dataList;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+
+
+}
