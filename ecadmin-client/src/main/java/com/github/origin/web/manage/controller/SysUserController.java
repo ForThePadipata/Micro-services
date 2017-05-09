a93755dac9e1cfcde3372b94c6e9e749f5a6cab5
zhuguidongChina <TZzhuguidong@outlook.com>
2017-05-09 18:09:50 +0800

    添加swagger2 API注释

--- ecadmin-client/src/main/java/com/github/origin/web/manage/controller/SysUserController.java
+++ ecadmin-client/src/main/java/com/github/origin/web/manage/controller/SysUserController.java
@@ -3,6 +3,8 @@ package com.github.origin.web.manage.controller;
 import com.github.origin.common.CommonModel;
 import com.github.origin.entity.SysUserDO;
 import com.github.origin.web.manage.service.SysUserClient;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +19,8 @@ public class SysUserController {
 	@Autowired
 	SysUserClient sysUserClient;
 
+	@ApiOperation(value = "系统管理员登录", notes = "管理员使用账户跟密码登录管理系统")
+	@ApiImplicitParam(name = "sysUserDO", value = "系统管理员实体sysUserDO")
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public CommonModel login(SysUserDO sysUserDO){
 		System.out.println("请求成功");
