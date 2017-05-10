152f16d8208827cbc440d7d6236e4cc0bab10f6c
zhuguidongChina <TZzhuguidong@outlook.com>
2017-05-10 18:13:08 +0800

    更新监控系统配置

--- ec-monitoring/src/main/java/com/github/origin/MonitoringApplication.java
+++ ec-monitoring/src/main/java/com/github/origin/MonitoringApplication.java
@@ -2,12 +2,14 @@ package com.github.origin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 
 @SpringBootApplication
-@EnableTurbineStream
+@EnableDiscoveryClient
+@EnableTurbine
 @EnableHystrixDashboard
 public class MonitoringApplication {
 
