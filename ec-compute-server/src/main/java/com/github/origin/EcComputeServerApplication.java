84c34356166fce8d56185fecf0c39f85df742174
zhuguidongChina <TZzhuguidong@outlook.com>
2017-05-12 17:43:04 +0800

    修改spring boot服务器为undertow 添加配置中心配置

--- ec-compute-server/src/main/java/com/github/origin/EcComputeServerApplication.java
+++ ec-compute-server/src/main/java/com/github/origin/EcComputeServerApplication.java
@@ -3,9 +3,11 @@ package com.github.origin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableHystrix
 public class EcComputeServerApplication {
 
 	public static void main(String[] args) {
