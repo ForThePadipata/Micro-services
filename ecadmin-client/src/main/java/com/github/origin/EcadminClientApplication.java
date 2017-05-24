f38e16f2304f01280b4ca8838ed5790bcd31a8f7
zhuguidongChina <TZzhuguidong@outlook.com>
2017-05-24 18:08:08 +0800

    优化配置

--- ecadmin-client/src/main/java/com/github/origin/EcadminClientApplication.java
+++ ecadmin-client/src/main/java/com/github/origin/EcadminClientApplication.java
@@ -1,14 +1,26 @@
 package com.github.origin;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class EcadminClientApplication {
+
+	/**
+	 * 开启全局Feign客户端日志级别
+	 *
+	 * @return
+	 */
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcadminClientApplication.class, args);
