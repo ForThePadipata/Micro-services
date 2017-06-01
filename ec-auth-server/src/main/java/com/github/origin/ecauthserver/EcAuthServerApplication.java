1fd6649c42540d36359f415a2ba9d16294da9485
zhuguidongChina <TZzhuguidong@outlook.com>
2017-06-01 18:10:15 +0800

    添加outh2授权模块

---
+++ ec-auth-server/src/main/java/com/github/origin/ecauthserver/EcAuthServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.origin.ecauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcAuthServerApplication.class, args);
+	}
+}
