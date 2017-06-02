c260e1687022b5d940e421f8bf3783195f44276f
zhuguidongChina <TZzhuguidong@outlook.com>
2017-06-02 18:03:08 +0800

    添加授权服务注释

--- ec-auth-server/src/main/java/com/github/origin/ecauthserver/EcAuthServerApplication.java
+++ ec-auth-server/src/main/java/com/github/origin/ecauthserver/EcAuthServerApplication.java
@@ -2,8 +2,10 @@ package com.github.origin.ecauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
+@EnableAuthorizationServer
 public class EcAuthServerApplication {
 
 	public static void main(String[] args) {
