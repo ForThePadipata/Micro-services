ec06305bc0b2f2b38beb8fe6652e916b8322ee97
zhuguidongChina <TZzhuguidong@outlook.com>
2017-06-30 09:34:43 +0800

    添加Mybatis整合 一些代码注释

---
+++ ec-user-server/src/main/java/com/github/origin/server/dao/UserMapper.java
@@ -0,0 +1,7 @@
+package com.github.origin.server.dao;
+
+/**
+ * Created by Zhu on 2017/6/28.
+ */
+public interface UserMapper {
+}
---
+++ ec-user-server/src/test/java/com/github/origin/dao/UserMapperTest.java
@@ -0,0 +1,7 @@
+package com.github.origin.dao;
+
+/**
+ * Created by Zhu on 2017/6/28.
+ */
+public class UserMapperTest {
+}
